cd31f878b31dd4af092816334880db692936ee11
Chris <saveen2112@gmail.com>
2014-12-07 16:34:08 -0700

    moe stuff. add first RGB test

--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -1,27 +1,2 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Schemes', function() {
-  // Might use a resource here that returns a JSON array
-
-
-  var schemes = [
-    { id: "sunset", name: 'Sunset' },
-    { id: 1, name: 'Aqua' },
-    { id: 2, name: 'Pastels' },
-    { id: 3, name: 'High Energy' },
-    { id: 4, name: 'Sandy Beach' }
-  ];
-
-  return {
-    all: function() {
-      return schemes;
-    },
-    get: function(schemeId) {
-      // Simple index lookup
-      return schemes[schemeId];
-    }
-  }
-});
